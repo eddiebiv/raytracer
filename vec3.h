e69e4ccbb4cbfe47a472340d2d109ef083b47000
eddiebiv <eddiebrenmark@gmail.com>
2023-06-04 23:25:30 -0500

    commit

--- vec3.h
+++ vec3.h
@@ -53,7 +53,6 @@ class vec3 {
 using point3 = vec3;   // 3D point
 using color = vec3;    // RGB color
 
-#endif
 
 
 // vec3 Utility Functions
@@ -100,3 +99,5 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
+
+#endif
